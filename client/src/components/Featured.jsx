1c055426f94bc254b36c309c570f9cfeb192a1ca
Bryan Lecza <blecza@travelers.com>
2025-03-19 10:52:57 -0400

    basic featured component

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -1,29 +1,33 @@
 import { useState, useEffect } from 'react'
 
 export const Featured = () => {
-    const [featuredProducts, setFeaturedProducts] = useState({})
+    const [featuredProducts, setFeaturedProducts] = useState([])
 
+    useEffect(() => {
+
+    })
     useEffect(()=>{
         const getProductData = async () => {
-            const product_data = await fetch(import.meta.env.VITE_API_URL/products)
-            const json = await product_data.json()
-            setFeaturedProducts(json)
+            try {
+                console.log("fetching data...")
+                const product_data = await fetch(`${import.meta.env.VITE_API_URL}/products`)
+                const json = await product_data.json()
+                const featured_product_data = json.slice(0, 3)
+                setFeaturedProducts(featured_product_data)
+            } catch(error) {
+                console.error(error)
+            }
         }
-
         getProductData()
     }, [])
-
+    
     return(
         <>
+            <h2>Featured Products!</h2>
+            <ul>
+                {console.log(featuredProducts)}
+                {featuredProducts.map((product) => (<li key={product._id}>{product.name}</li>))}
+            </ul>
         </>
     )
-    
-    // return(
-    //     <>
-    //         <h2>Featured Products!</h2>
-    //         <ul>
-    //             {featuredProducts.map((product) => (<li key={product._id}>{}</li>))}
-    //         </ul>
-    //     </>
-    // )
 }
